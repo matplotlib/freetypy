ecc66f48ec75e82d64cd757b2486fa30867661fa
Michael Droettboom <mdboom@gmail.com>
2015-10-29 13:21:00 -0400

    Update copyright years

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'freetypy'
-copyright = u'2013, Michael Droettboom'
+copyright = u'2015, Michael Droettboom'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- lib/freetypy/tests/test_sfntnames.py
+++ lib/freetypy/tests/test_sfntnames.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2013, Michael Droettboom All rights reserved.
+# Copyright (c) 2015, Michael Droettboom All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
